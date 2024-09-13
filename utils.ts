cd23153a3254270e71f18f03302d5893255819f3
JimGitFE <rymsjfjrys@gmail.com>
2024-09-13 01:37:41 -0300

    refactor: dependencies install at same time & add isValidPath: verify string for path & add: askUser fpr text input

--- utils.ts
+++ utils.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { execSync } from "child_process";
+import readline from 'readline';
 
 const runCommand = (command: string) => {
     try {
@@ -12,9 +13,7 @@ const runCommand = (command: string) => {
 };
 
 const installDependencies = (dependencies: string[]) => {
-    dependencies.forEach((dependency) => {
-        runCommand(`npm i -D ${dependency}@latest`);
-    });
+    runCommand(`npm i -D ${dependencies.join("@latest ")}`);
 }
 
 const packageJsonPath = path.resolve(process.cwd(), 'package.json');
@@ -37,11 +36,39 @@ function addScriptToPackageJson(scriptName: string, scriptCommand: string) {
 
     // Write changes back to package.json
     fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2), 'utf8');
-    console.log(`Script "${scriptName}" added to package.json.`);
 }
 
-function writeFileInRoot(file: any) {    
-    fs.writeFileSync(path.resolve(process.cwd(), 'nodemon.json'), JSON.stringify(file, null, 2), 'utf8');
+function writeFileInRoot(fileName: string,file: any) {    
+    fs.writeFileSync(path.resolve(process.cwd(), fileName), JSON.stringify(file, null, 2), 'utf8');
 }
 
-export { installDependencies, addScriptToPackageJson, writeFileInRoot };
+function askUser(title: string, reply: string, defaultAnswer: string, callback: (answer: string) => void) {
+        // Create readline interface
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        // Ask user for file path
+        rl.question(title, (filePath) => {
+            // Use default if no path provided
+            if (!filePath) {
+              filePath = defaultAnswer;
+            }
+            callback(filePath)
+            console.log(reply);
+            // Rest of your code here...
+          
+            rl.close();
+          });
+}
+
+function isValidPath(filePath: string): boolean {
+    try {
+      path.parse(filePath);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+export { installDependencies, addScriptToPackageJson, writeFileInRoot, askUser, isValidPath };
